046e9dc02fc0d789975fe0d86bcaf9284ff4c81f
Varis Darasirikul <varis.dara@gmail.com>
2020-07-25 18:35:14 +0800

    feature/display_news_card: Finish display

---
+++ src/app/components/NewsCard/NewsCard.component.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import moment from 'moment'
+import parse from 'html-react-parser'
+
+import styles from './NewsCard.module.scss'
+
+interface INewsCard {
+  sourceName: string
+  published: string
+  image: string
+  title: string
+  description: string
+}
+
+export const NewsCard: React.FC<INewsCard> = ({
+  sourceName,
+  published,
+  image,
+  title,
+  description,
+}) => {
+  return (
+    <div className={`card ${styles.container}`}>
+      <div className="card-content">
+        <div className="media">
+          <div className="media-left">
+            <div className={styles.avartar}>
+              <strong className="text-white is-size-4">{sourceName[0]}</strong>
+            </div>
+          </div>
+          <div className="media-content">
+            <p className="title is-size-6">{sourceName}</p>
+            <p className="subtitle is-size-6 text-date">
+              {moment(published).format('YYYY-MM-DD hh:mm')}
+            </p>
+          </div>
+        </div>
+
+        <div className={styles.image}>{image && <img src={image} alt={title} />}</div>
+
+        <div className="content p-t-15">
+          <p className={`is-size-4 m-b-15 ${styles.title}`}>{title}</p>
+          <p className={styles.description}>{parse(description)}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/app/containers/Feed/Feed.component.tsx
+++ src/app/containers/Feed/Feed.component.tsx
@@ -2,9 +2,11 @@ import React, { useEffect } from 'react'
 import { connect, ConnectedProps } from 'react-redux'
 
 import { fetchNews } from 'src/app/store/feed/actions'
+import { IFeedEntities } from 'src/app/store/feed/types'
 import { RootState } from 'src/app/store/root'
 import { Loading } from 'src/app/components/Loading/Loading.component'
 import { Header } from 'src/app/components/Header/Header.component'
+import { NewsCard } from 'src/app/components/NewsCard/NewsCard.component'
 
 export const Feed: React.FC<ConnectedProps<typeof connector>> = ({ feed, fetchNews }) => {
   useEffect(() => {
@@ -13,10 +15,43 @@ export const Feed: React.FC<ConnectedProps<typeof connector>> = ({ feed, fetchNe
     })
   }, [])
 
+  const { entities, loading } = feed
+
   return (
     <div>
       <Header />
-      {feed.loading && <Loading />}
+      {loading && (
+        <div className="p-t-30">
+          <Loading />
+        </div>
+      )}
+
+      {!loading && (
+        <div className="container p-t-30 p-b-30">
+          <div className="columns is-multiline">
+            {entities &&
+              ((Object.keys(entities) as unknown) as Array<keyof IFeedEntities>).map(key => {
+                return entities[key].map(entity => {
+                  const { source, title, description, urlToImage, publishedAt } = entity
+                  return (
+                    <div
+                      key={entity.title}
+                      className="column is-4-desktop is-6-tablet is-12-mobile"
+                    >
+                      <NewsCard
+                        sourceName={source.name}
+                        published={publishedAt}
+                        image={urlToImage}
+                        title={title}
+                        description={description}
+                      />
+                    </div>
+                  )
+                })
+              })}
+          </div>
+        </div>
+      )}
     </div>
   )
 }
